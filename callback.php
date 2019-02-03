1dbe944efd2c6462b8125f7da91849ffa8ece6db
tempsteve <tempsteve1024@gmail.com>
2019-02-03 18:19:43 +0800

    1st

---
+++ callback.php
@@ -0,0 +1,54 @@
+<?php
+require_once "Facebook/autoload.php";
+
+$fb = new \Facebook\Facebook([
+    'app_id' => 'Your app id',
+    'app_secret' => 'Your secret key',
+    'default_graph_version' => 'v2.12' //Better use the latest version
+]);
+$helper = $fb->getRedirectLoginHelper();
+
+// The cases of login failed conditions
+try {
+	$accessToken = $helper->getAccessToken();
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+	// When Graph returns an error
+	echo 'Graph returned an error: ' . $e->getMessage();
+	exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+	// When validation fails or other local issues
+	echo 'Facebook SDK returned an error: ' . $e->getMessage();
+	exit;
+}
+
+if (! isset($accessToken)) {
+	if ($helper->getError()) {
+		header('HTTP/1.0 401 Unauthorized');
+		echo "Error: " . $helper->getError() . "\n";
+		echo "Error Code: " . $helper->getErrorCode() . "\n";
+		echo "Error Reason: " . $helper->getErrorReason() . "\n";
+		echo "Error Description: " . $helper->getErrorDescription() . "\n";
+	} else {
+		header('HTTP/1.0 400 Bad Request');
+		echo 'Bad request';
+	}
+	exit;
+}
+
+try {
+	// Returns a `Facebook\FacebookResponse` object
+	$response = $fb->get('/me?fields=id,name,email', $accessToken);
+} catch (Facebook\Exceptions\FacebookResponseException $e) {
+	echo 'Graph returned an error: ' . $e->getMessage();
+	exit;
+} catch (Facebook\Exceptions\FacebookSDKException $e) {
+	echo 'Facebook SDK returned an error: ' . $e->getMessage();
+	exit;
+}
+
+// Login successed, then get the user's data
+$user = $response->getGraphUser();
+
+// This is the user's Facebook account that you can post to your login page.
+$account = $user['email'];
+?>
